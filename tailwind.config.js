a4d6375306e6e2ffcc9683000d98c0389398fe94
Kris <92295923+Kris248@users.noreply.github.com>
2025-04-04 19:51:49 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,24 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        float: 'float 1s ease-in-out infinite',
+        fadeInOut: 'fadeInOut 5s ease-in-out forwards',
+      },
+      keyframes: {
+        float: {
+          '0%, 100%': { transform: 'translateY(0)' },
+          '50%': { transform: 'translateY(-20px)' },
+        },
+        fadeInOut: {
+          '0%': { opacity: 0 },
+          '10%': { opacity: 1 },
+          '90%': { opacity: 1 },
+          '100%': { opacity: 0 },
+        },
+      },
+    }    
   },
   plugins: [],
 };
